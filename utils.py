35ea88a717308d46ba026ce00ebd6329b3d0d4bb
mistergold1 <159539139+mistergold1@users.noreply.github.com>
2026-01-29 14:46:16 +0200

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,9 @@
+import hashlib
+
+
+def hash_password(password: str) -> str:
+    return hashlib.sha256(password.encode()).hexdigest()
+
+
+def check_password(password: str, password_hash: str) -> bool:
+    return hash_password(password) == password_hash
