1723a2805690c8cb994ad072712293be04b99c0c
mistergold1 <159539139+mistergold1@users.noreply.github.com>
2026-01-29 14:51:19 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -8,4 +8,9 @@ def hash_password(password: str) -> str:
 def check_password(password: str, password_hash: str) -> bool:
     return hash_password(password) == password_hash
 
-    # TODO: add password strength validation
+def add(self, username, password_hash):
+    # TODO: prevent weak usernames
+    self.users[username] = {
+        "username": username,
+        "password": password_hash
+    }
